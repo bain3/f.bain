58360a02a569799938fd22d182f86c4ac35a0c71
bain3 <bain3@users.noreply.github.com>
2020-11-25 14:09:54 +0100

    added encryption, woo!

--- api/app/main.py
+++ api/app/main.py
@@ -1,32 +1,82 @@
 """
 This is basically all of f.bain. Very simple.
 """
+import binascii
+from random import choice
 
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import FileResponse
-import uuid
-import hashlib
+from fastapi import FastAPI, HTTPException, Body, Header, Request
+from starlette.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, Response
 import aiofiles
+import os
 import redis as redis_
 from .CONSTANTS import REDIS
+import json
+from base64 import b64decode
 
 redis = redis_.Redis(host=REDIS['host'], port=REDIS['port'], db=REDIS['db'], password=REDIS['password'])
+redis.set("initial", "something")
 
 app = FastAPI()
 
 
 @app.post("/")
-async def create_file(file: UploadFile = File(None)):
-    uuid_ = hashlib.sha1(uuid.uuid4().bytes).hexdigest()[:8]
-    redis.set("filename-"+str(uuid_), file.filename)
-    async with aiofiles.open("/mount/"+str(uuid_), 'wb+') as f:
-        await f.write(await file.read())
-    return {"uuid": str(uuid_), "filename": file.filename}
+async def create_file(request: Request, x_metadata: str = Header("")):
+    try:
+        if not x_metadata:
+            raise binascii.Error
+        metadata = b64decode(x_metadata)
+    except binascii.Error:
+        raise HTTPException(status_code=400, detail="X-Metadata header badly formed.")
+    # get a new uuid
+    uuid = 'initial'
+    while redis.exists(uuid):
+        uuid = ''.join([choice("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$–_.+!*‘(),") for i in
+                        range(5)])
+    print(metadata)
+    redis.set("metadata-" + uuid, metadata)
+    async with aiofiles.open("/mount/upload/" + uuid.encode().hex(), 'wb+') as f:
+        await f.write(await request.body())
+    return {"uuid": uuid}
 
 
-@app.get("/{uuid_}")
-async def get_file(uuid_: str):
-    filename = redis.get("filename-"+uuid_)
-    if not filename:
+@app.get("/{uuid}")
+async def get_file(uuid: str):
+    meta = redis.get("metadata-" + uuid)
+    if not meta:
+        print("meta not found")
         raise HTTPException(status_code=404, detail="File was not found.")
-    return FileResponse("/mount/"+uuid_, filename=filename.decode())
+    if not os.path.exists("/mount/upload/" + uuid.encode().hex()):
+        print("file not found")
+        redis.delete("metadata-" + uuid)
+        raise HTTPException(status_code=404, detail="File was not found.")
+    async with aiofiles.open("/mount/static/index.html", 'r') as f:
+        return HTMLResponse(await f.read())
+
+
+@app.get("/{uuid}/meta")
+def get_meta(uuid: str):
+    meta = redis.get("metadata-" + uuid)
+    if not meta:
+        raise HTTPException(status_code=404, detail="No meta was found.")
+
+    # checking if the meta was formed correctly
+    try:
+        meta = json.loads(meta)
+        if set(meta.keys()) != {"salt", "filename"}:
+            print(meta)
+            raise json.JSONDecodeError("", "", 0)
+    except json.JSONDecodeError:
+        # Deleting malformed metadata and returning error
+        redis.delete("metadata-" + uuid)
+        raise HTTPException(status_code=422, detail="Meta was malformed.")
+
+    return meta
+
+
+@app.get("/{uuid}/raw")
+async def get_raw(uuid: str):
+    if not os.path.exists("/mount/upload/" + uuid.encode().hex()):
+        raise HTTPException(status_code=404, detail="File was not found.")
+    async with aiofiles.open("/mount/upload/" + uuid.encode().hex(), 'rb') as f:
+        return Response(await f.read(), media_type="application/octet-stream", status_code=200)
--- static/script.js
+++ static/script.js
@@ -1,48 +1,120 @@
+// all available characters in a url
+let base73 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$–_.+!*‘(),";
+let strength = 12;
+
 function inputHandler(ev) {
     ev.preventDefault();
-    let formData = new FormData();
     let el = document.getElementById('inp');
-    formData.append('file', el.files[0]);
-    sendRequest(formData);
+    // console.log(el.files[0]);
+    sendRequest(el.files[0]);
 }
 
 function dropHandler(ev) {
-    console.log(ev);
+    // console.log(ev);
 
     // Prevent default behavior (Prevent file from being opened)
     ev.preventDefault();
-    let formData = new FormData();
 
+    let toSend;
     if (ev.dataTransfer.items) {
-        // Use DataTransferItemList interface to access the file(s)
         let file = ev.dataTransfer.items[0].getAsFile();
-        console.log(file);
-        formData.append('file', file);
+        // console.log(file);
+        toSend = file;
     } else {
-        // Use DataTransfer interface to access the file(s)
-        console.log(ev.dataTransfer.files[0]);
-        formData.append('file', ev.dataTransfer.files[0]);
+        // console.log(ev.dataTransfer.files[0]);
+        toSend = ev.dataTransfer.files[0];
     }
-    console.log(formData);
-    sendRequest(formData);
+    // console.log(toSend);
+
+    sendRequest(toSend);
 }
 
-function sendRequest(formData) {
+function random_chars(number) {
+    let array = sjcl.random.randomWords(number, 0);
+    // console.log(array);
+    let output = "";
+    for (let i = 0; i < array.length; i++) {
+        output += base73[Math.abs(array[i]%73)];
+    }
+    return output
+}
+
+async function encrypt(file) {
+
+    // generate random password in base73
+    let password;
+    do {
+        password = random_chars(strength);
+    } while ([".", ",", ")"].includes(password[password.length-1])); // last character cannot be ) , or ., discord doesn't like it
+
+    // password stretching to 512 bits and strengthening
+    let salt = sjcl.random.randomWords(2,0);
+    let strengthened = sjcl.misc.pbkdf2(password, salt, 50000, 768);
+
+    // create aes instance using our generated key (first 256 bits)
+    let prp = new sjcl.cipher.aes(strengthened.splice(0, 8));
+
+    let iv = strengthened.splice(0,8); // setting iv (second 256 bits)
+
+    let output_blob = new Blob([]); // preparing output blob, needs to be blob to not crash the browser
+    let offset = 0;
+    while (offset < file.size) {
+        // getting a block of data (5mb)
+        let block_data = new Int8Array(await file.slice(offset, offset+5242880).arrayBuffer());
+        // getting iv IN THE RIGHT FORM, UGHHH
+        let new_iv = sjcl.random.randomWords(8,0);
+        let new_iv_int8 = new Int8Array(new Int32Array(new_iv).buffer);
+
+        // concatenating iv and data into the same block
+        let block = new Int8Array(new_iv_int8.byteLength+block_data.byteLength);
+        block.set(new_iv_int8, 0);
+        block.set(block_data, new_iv_int8.byteLength);
+
+        // encryption of the block using aes in gcm mode
+        let cipher = sjcl.mode.gcm.encrypt(prp, sjcl.codec.arrayBuffer.toBits(block.buffer), iv);
+
+        // this takes a shit ton of time, sadly can't do anything about that
+        // (please help if you know how to work around this issue)
+        output_blob = new Blob([output_blob, sjcl.codec.arrayBuffer.fromBits(cipher, false)]);
+
+        offset += 5242880;
+        iv = new_iv; // changing the iv for the next block to not weaken the encryption
+        document.getElementsByClassName('progress-value')[0].style.width = offset/file.size*50+'%';
+    }
+
+    // encrypt filename
+    let name_iv = strengthened.splice(0, 8); // using third 256 bits of our stretched key
+    let enc_bits = sjcl.mode.gcm.encrypt(prp, sjcl.codec.utf8String.toBits(file.name), name_iv);
+    let filename = sjcl.codec.base64.fromBits(enc_bits, false);
+
+    return {output_blob, key: password, salt, filename}
+}
+
+async function sendRequest(file) {
+
     let error_el = document.getElementById('errors');
     let success_el = document.getElementById('success');
     let uuid_el = document.getElementById('uuid');
+    let key_el = document.getElementById('key');
     let upload_icon = document.getElementById('upload-icon');
     let input_el = document.getElementById('inp');
-    let progress_el = document.getElementsByClassName('progress')[0]
-    console.log(formData);
+    let progress_el = document.getElementsByClassName('progress')[0];
+    input_el.disabled = true;
+    input_el.style.zIndex = '-1';
+    progress_el.style.visibility = 'initial';
 
-    var xhr = new XMLHttpRequest();
+    // encrypt the file
+    let encrypted;
+    encrypted = await encrypt(file);
 
+    // send post request with data and metadata
+    const xhr = new XMLHttpRequest();
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === this.DONE) {
             if (this.status === 200) {
                 let json = JSON.parse(this.responseText);
                 uuid_el.innerText = json.uuid;
+                key_el.innerText = encrypted.key;
                 upload_icon.hidden = true;
                 success_el.hidden = false;
             } else {
@@ -54,16 +126,17 @@ function sendRequest(formData) {
         }
     });
 
+    // update progress bar
     xhr.upload.addEventListener("progress", function (p) {
-        document.getElementsByClassName('progress-value')[0].style.width = p.loaded/p.total*100+'%'
+        document.getElementsByClassName('progress-value')[0].style.width = 50+(p.loaded/p.total*86)+'%';
     })
 
     xhr.open("POST", "http://localhost:3333/");
+    xhr.setRequestHeader("Content-Type", "application/octet-stream");
 
-    xhr.send(formData);
-    progress_el.style.visibility = 'initial';
-    input_el.disabled = true;
-    input_el.style.zIndex = '-1';
+    // setting metadata header to send salt and file name encoded in json, then in base64
+    xhr.setRequestHeader("X-Metadata", window.btoa(JSON.stringify({filename: encrypted.filename, salt: encrypted.salt})));
+    xhr.send(encrypted.output_blob);
 }
 
 function copyToClipboard() {

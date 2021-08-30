76979817bc9b44bdfcf544ac59589c3b1f0c9f99
bain3 <31798786+bain3@users.noreply.github.com>
2021-08-30 14:03:03 +0200

    remove unnecessary switch in favor of if statements

--- static/progress.js
+++ static/progress.js
@@ -1,8 +1,9 @@
 class Progress {
+    colors = {"error": "--red", "success": "--green", "neutral": "--bright"}
+
     constructor(status, progress_value_el, status_text_el, progress_change_func) {
         this.progress_value = progress_value_el;
         this.status_text = status_text_el;
-        this.current_status = status;
         this.progress_change_func = progress_change_func || this.pcf;
         this.update(status);
     }
@@ -12,36 +13,16 @@ class Progress {
     }
 
     update(status) {
-        for (let key of Object.keys(status)) {
-            switch (key) {
-                case "status":
-                    let valid = true;
-                    switch (status.status) {
-                        case "error":
-                            this.progress_value.style.background = "var(--red)";
-                            this.progress_value.style.width = this.progress_change_func(1);
-                            break;
-                        case "success":
-                            this.progress_value.style.background = "var(--green)";
-                            this.progress_value.style.width = this.progress_change_func(1);
-                            break;
-                        case "neutral":
-                            this.progress_value.style.background = "var(--bright)";
-                            break;
-                        default:
-                            valid = false;
-                    }
-                    if (valid) this.current_status.status = status.status;
-                    break;
-                case "statusText":
-                    this.current_status.statusText = status.statusText;
-                    this.status_text.innerText = status.statusText;
-                    break;
-                case "progress":
-                    this.current_status.progress = status.progress;
-                    this.progress_value.style.width = this.progress_change_func(status.progress);
-                    break;
-            }
+        if (status === undefined) return;
+        if (status.progress !== undefined) {
+            this.progress_value.style.width = this.progress_change_func(status.progress);
+        }
+        if (status.statusText !== undefined) {
+            this.status_text.innerText = status.statusText;
+        }
+        if (status.status !== undefined) {
+            if (status.status !== "neutral") this.progress_value.style.width = this.progress_change_func(1);
+            this.progress_value.style.background = `var(${this.colors[status.status]})`;
         }
     }
 }
